a765dfb63093eec17f05981885161d5f1067233c
Josh Paulsen <josh.paulsen@berkeley.edu>
2012-04-30 15:31:06 -0700

    fixed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-admin = User.create!(:name => "admin", :password => "a", :privilege => "admin", :email => "admin@admin.com")
+admin = User.new(:first_name => "Admin", :last_name => "Toast", :privilege => "admin", :email => "admin")
 admin.active = true
+admin.verified = true
+admin.set_encrypted_password("111111")
 admin.save
