83d3315a19ea4963b5526f8376845d1b74212c4b
Josh Paulsen <josh.paulsen@berkeley.edu>
2012-05-03 01:21:48 -0700

    bug in service area forms

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,6 +2,7 @@ class Location < ActiveRecord::Base
   # name:string
   # address:string
   # active:boolean
+  default_scope :order => 'id ASC'
   
   scope :active, where(:active => true)
   scope :inactive, where(:active => false)
